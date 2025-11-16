c6a4a4583e8c2d10b150f7c28d90a3b62a94c3b1
VoDylan <vdylan24@gmail.com>
2025-11-16 16:10:23 -0500

    merged

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
+from linkstate import GraphLS
 
 graph = {'A' : {'B' : 2, 'E' : 6, 'D' : 5}, 'B' : {'A' : 2, 'C' : 1}, 'C' : {'B' : 1, 'E' : 3, 'H' : 1}, 'D' : {'A' : 5, 'E' : 4, 'F' : 2}, 'E' : {'A' : 6, 'D' : 4, 'F' : 1, 'C' : 3}, 'F' : {'D' : 2, 'E' : 1, 'G' : 3}, 'G' : {'F' : 7, 'H' : 3}, 'H' : {'G' : 3, 'C' : 1}}
+
+
 
 # Get the first argument (python3 main.py <graph_file>)
 try:
@@ -46,3 +49,8 @@ except:
                     graph[y][x] = int(cost)
         except:
             print("Please enter a valid input.")
+
+
+graphLS = GraphLS(graph)
+graphLS.dijkstra("A")
+graphLS.dijkstra("X")
